3ad0728516309f5b0d9a9ba9a2d1d0d776599e29
JochoVoled <johannes@smidelov.com>
2016-09-21 10:31:21 +0200

    Added namespaces, with temporary files

---
+++ BestOfBrands/BestOfBrands/DatabaseRepository/tmpDatabaseRepoFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestOfBrands.DatabaseRepository
+{
+    public class tmpDatabaseRepoFile
+    {
+    }
+}
---
+++ BestOfBrands/BestOfBrands/Interfaces/ITempInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestOfBrands.Interfaces
+{
+    public class ITempInterface
+    {
+    }
+}
