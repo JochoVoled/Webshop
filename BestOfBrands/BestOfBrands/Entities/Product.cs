688438322482637704f338388db693a69bc85a0f
JochoVoled <johannes@smidelov.com>
2016-09-22 11:30:15 +0200

    Removed tempEntities, added product class

---
+++ BestOfBrands/BestOfBrands/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using BestOfBrands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BestOfBrands.Enums;
+
+namespace BestOfBrands.Entities
+{
+    public class Product : IProduct
+    {
+        public IProductCategory Category { get; set; }
+        public string Description { get; set; }
+        public bool IsChild { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ProductNumber { get; set; }
+        public string Size { get; set; }
+        public int StockAmount { get; set; }
+        public EGender TargetConsumer { get; set; }
+
+        // todo Product constructor
+    }
+}
