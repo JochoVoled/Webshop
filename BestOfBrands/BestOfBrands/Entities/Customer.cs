95f8d0fc48e2a4d49f5e50248d7984bb2ddd1151
aeklundh <eklundh.albin@gmail.com>
2016-09-22 14:40:07 +0200

    added counter, modified customer and person

---
+++ BestOfBrands/BestOfBrands/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using BestOfBrands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestOfBrands.Entities
+{
+    public class Customer : Person, ICustomer
+    {
+        public int ID { get; set; }
+
+        Customer(string address, string email, string name, string password, string phoneNumber, string username)
+        {
+            Address = address;
+            Email = email;
+            Name = name;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Username = username;
+            //todo add ID w/ counter
+        }
+
+        public bool EditOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
