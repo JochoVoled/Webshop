f0ebb38b38d8ed5ca4df0a5913c12fcd96b48efb
JochoVoled <johannes@smidelov.com>
2016-09-27 11:34:43 +0200

    Wrote constructor for Product, with default parameter values

--- BestOfBrands/BestOfBrands/Entities/Product.cs
+++ BestOfBrands/BestOfBrands/Entities/Product.cs
@@ -1,18 +1,13 @@
 ﻿using BestOfBrands.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using BestOfBrands.Enums;
 
 namespace BestOfBrands.Entities
 {
     public class Product : IProduct
     {
+        public string Name { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public bool IsChild { get; set; }
-        public string Name { get; set; }
         public decimal PriceSell { get; set; }
         public decimal PriceBuy { get; set; }
         public string ProductNumber { get; set; }
@@ -20,6 +15,17 @@ namespace BestOfBrands.Entities
         public int StockAmount { get; set; }
         public string Gender { get; set; }
 
-        // todo Product constructor
+        public Product(string Name, string Category, string Description, bool IsChild = false, decimal SellPrice = 0, decimal PurchedPrice = 0, string Size = "M", int StockAmount = 1, string Gender = "Unisex")
+        {
+            this.Name = Name;
+            this.Category = Category; /* osv */
+            this.Description = Description;
+            this.IsChild = IsChild;
+            PriceSell = SellPrice;
+            PriceBuy = PurchedPrice;
+            this.Size = Size;
+            this.StockAmount = StockAmount;
+            this.Gender = Gender;
+        }
     }
 }
