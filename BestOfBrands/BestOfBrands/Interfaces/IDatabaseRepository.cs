72458893a80ddc0d5c50cf6f13cf464bd5600770
Ester Bergström <Ester Bergstrm@DESKTOP-UNGVHJO>
2016-09-27 13:23:45 +0200

    IDatabaseRepository och ProductDatabaseRepository

---
+++ BestOfBrands/BestOfBrands/Interfaces/IDatabaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestOfBrands.Interfaces
+{
+    public interface IDatabaseRepository<T>
+    {
+        void Save(T item);
+        List<T> GetAll();
+        int NextItemID();
+    }
+}
