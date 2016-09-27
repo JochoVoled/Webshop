d877db9a86f48c8a261f3caeb98c0de88d8550cf
JochoVoled <johannes@smidelov.com>
2016-09-27 14:08:39 +0200

    Implemented Cart and Order classes

---
+++ BestOfBrands/BestOfBrands/Entities/Cart.cs
@@ -0,0 +1,86 @@
+﻿using BestOfBrands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestOfBrands.Entities
+{
+    public class Cart : ICart
+    {
+        public List<IProduct> productList { get; set; }
+
+        public bool Add(IProduct product)
+        {
+            try
+            {
+                productList.Add(product);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }            
+        }
+
+        public bool CheckOut()
+        {
+            try
+            {
+                // todo open CheckOut view, open this list
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Clear()
+        {
+            try
+            {
+                productList.Clear();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public decimal CountPrice()
+        {
+            return productList.Sum(x => x.PriceSell);
+        }
+
+        public bool PlaceOrder()
+        {
+            try
+            {
+                Order newOrder = new Order() {
+                    orderedProducts = productList,
+                    /*todo find customer ID so it can be set*/
+                };
+                Clear();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(IProduct product)
+        {
+            try
+            {
+                productList.Remove(product);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BestOfBrands/BestOfBrands/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿using BestOfBrands.Interfaces;
+using System.Collections.Generic;
+
+namespace BestOfBrands.Entities
+{
+    public class Order : IOrder
+    {
+        public int customerID { get; }
+        public List<IProduct> orderedProducts { get; set; }
+    }
+}
